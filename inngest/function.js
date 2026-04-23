21a0b05e38e37094e0db2c50531a845559cfc633
quick--lerner <saha20428@gmail.com>
2026-04-23 20:38:16 +0400

    fix prisma dynamic import for vercel

--- inngest/function.js
+++ inngest/function.js
@@ -3,22 +3,23 @@ import prisma from '../lib/prisma'
 
 // inngest function to save user data to database
 export const syncUserCreation = inngest.createFunction(
-  { id: 'sync-user-create' },
-  { event: 'clerk/user.created' },
+  { id: "sync-user-create" },
+  { event: "clerk/user.created" },
   async ({ event }) => {
-    const { data } = event
+    const { data } = event;
+
+    const prisma = await getPrisma();
 
     await prisma.user.create({
       data: {
         id: data.id,
-        email: data.email_addresses[0].email_address,
-        name: `${data.first_name} ${data.last_name}`,
+        email: data.email_addresses?.[0]?.email_address || "",
+        name: `${data.first_name || ""} ${data.last_name || ""}`,
         image: data.image_url,
       },
-    })
+    });
   }
-)
-
+);
 // Inngest Function to update user data in database
 
 import { inngest } from './client'
@@ -46,14 +47,10 @@ export const syncUserUpdate = inngest.createFunction(
 )
 
 //  delete function
-
-
-
 import { inngest } from './client'
 import prisma from '../lib/prisma'
 
 // inngest function to delete user from database
-
 
 export const syncUserDeletion = inngest.createFunction(
   { id: 'sync-user-delete' },
